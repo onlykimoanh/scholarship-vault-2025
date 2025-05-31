a817ea407f49367154c79b1a69eab56e5b9a2e11
onlykimoanh <hi.oanhphan@gmail.com>
2025-05-31 16:19:20 +0700

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/scholarship-vault-2025/',
   server: {
     port: 3000,
   },
